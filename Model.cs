7be6c312b118b76847303b6be6ead316dd2df7b4
BrunoAzevedo801211 <167922811+BrunoAzevedo801211@users.noreply.github.com>
2024-04-24 21:18:43 +0100

    Add files via upload
    Primeira versão ccom delegate's e event's. Não tem comentarios.  Está muito encompleto.

---
+++ Model.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decktacular
+{
+	class Model
+	{
+		public delegate void Notificar();
+		public event Notificar DefinicoesCarregadas;
+		public event Notificar NovoJogoCriado;
+
+		public event Notificar EstadoAtualizado;
+		public event Notificar JogoGravado;
+
+		public event Notificar EstatisticasGravadas;
+
+		internal void CarregarDefinicoes()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void CriarNovoJogo()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void CartaClicada(int numCarta)
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void CartaLargada(int numCarta)
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void GravaJogo()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void AbreJogo()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void GravaEstatistica()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void PedirDefinicoes(ref string asDefinicoes)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decktacular
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
