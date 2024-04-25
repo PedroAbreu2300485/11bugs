dd749dc99a272590e7c8583cf2d46cdea34ba6a4
Bruno Azevedo <brunoazevedo@slice.pt>
2024-04-25 17:09:04 +0100

    Alte

--- Model.cs
+++ Model.cs
@@ -22,7 +22,7 @@ namespace Decktacular
 			Console.WriteLine("Carrega definicoes");
 			DefinicoesCarregadas?.Invoke();
 		}
-
+		 
 		internal void CriarNovoJogo()
 		{
 			Console.WriteLine("Cria novo jogo");
